38f1b0743133da78d90f64a95bbc1e04a81ce481
sxlx333 <knifeq8@gmail.com>
2024-12-08 20:34:42 +0200

    homepage design

--- client/src/layout/PublicLayout.jsx
+++ client/src/layout/PublicLayout.jsx
@@ -1,6 +1,6 @@
-import { Footer } from "../components/Footer";
-import { Header } from "../components/Header";
-import { Outlet } from "react-router-dom";
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+import { Outlet } from 'react-router-dom';
 
 export function PublicLayout() {
   return (
--- client/src/pages/public/Home.jsx
+++ client/src/pages/public/Home.jsx
@@ -1,22 +1,47 @@
-import { RegistrationForm } from "../../components/RegistrationForm";
+import { RegistrationForm } from '../../components/RegistrationForm';
+import styles from './Home.module.css';
+import { Link } from 'react-router-dom';
 
 export function Home() {
   return (
     <main>
-      <div className="container px-4">
-        <div className="row align-items-center g-lg-5 py-5">
-          <div className="col-lg-6 col-xl-7 text-center text-lg-start">
-            <h1 className="display-4 fw-bold lh-1 text-body-emphasis mb-3">
-              Vertically centered hero sign-up form
-            </h1>
-            <p className="fs-4">
-              Below is an example form built entirely with Bootstrap’s form
-              controls. Each required form group has a validation state that can
-              be triggered by attempting to submit the form without completing
-              it.
-            </p>
+      <div className={`container px-4 ${styles.twoSectionContainer}`}>
+        <div className={`row align-items-center g-lg-0 py-5`}>
+          <div
+            className={`col-lg-6 col-xl-6 text-center text-lg-start ${styles.homeRegistrationContainer}`}
+          >
+            <div className={styles.opacityBackground}>
+              <div className="homeRegisterTitle">
+                <h1 className="display-6 fw-bold lh-1 text-body-emphasis mb-3">
+                  Registruokitės
+                </h1>
+                <hr className="lineBreak"></hr>
+                <p className="fs-4">
+                  Prisijunkite prie socialinio tinklapio - Gandalizdis!
+                </p>
+                <p className="fs-4">
+                  Skaitykite ir kurkite Jums patinkantį turinį.
+                </p>
+              </div>
+
+              <div className="homeLoginLink">
+                <p className="fs-4">
+                  Ar jau esate prisiregistravęs vartotojas?
+                </p>
+
+                <Link
+                  to="/login"
+                  className="d-inline-flex link-body-emphasis text-decoration-none"
+                >
+                  <p className={`fs-4 ${styles.linkToLoginPara}`}>
+                    Prisijunkite čia
+                  </p>
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className="col-md-10 mx-auto col-lg-6 col-xl-5">
+
+          <div className="col-md-10 mx-auto col-lg-6 col-xl-6">
             <RegistrationForm />
           </div>
         </div>

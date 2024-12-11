93d5d3fbdd58ff9c57d1b3db59fcffb9d28e698b
sxlx333 <knifeq8@gmail.com>
2024-12-11 13:36:01 +0200

    footer styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,31 +5,32 @@ import style from './Footer.module.css';
 export function Footer() {
   return (
     <div className="container">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <p className="col-md-4 mb-0 text-body-secondary">
+      <footer className="d-flex align-items-center">
+        <p className="col-md-4 text-body-secondary">
           &copy; 2024 Gandalizdis, Corporation
         </p>
 
-        <a
-          href="/"
-          className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
-        >
+        <a href="/" className={`col-md-4 ${style.customFooterLogo}`}>
           <img src={logo} alt="Logo" className={style.logo} />
         </a>
 
-        <ul className="nav col-md-4 justify-content-end">
-          <li className="nav-item">
-            <Link to="/" className="nav-link px-2 text-body-secondary">
+        <ul className={`col-md-4 ${style.customFooterNav}`}>
+          {' '}
+          <li>
+            <Link to="/" className={style.customFooterLink}>
+              {' '}
               Pagrindinis
             </Link>
           </li>
-          <li className="nav-item">
-            <Link to="/faq" className="nav-link px-2 text-body-secondary">
+          <li>
+            <Link to="/faq" className={style.customFooterLink}>
+              {' '}
               Klausimai
             </Link>
           </li>
-          <li className="nav-item">
-            <Link to="/tos" className="nav-link px-2 text-body-secondary">
+          <li>
+            <Link to="/tos" className={style.customFooterLink}>
+              {' '}
               Paslaugos sąlygos
             </Link>
           </li>

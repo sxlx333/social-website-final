8aa18368f2c173e5936fe32deae92c6032780add
sxlx333 <knifeq8@gmail.com>
2024-12-13 21:20:28 +0200

    post feed background image

--- client/src/pages/user/Feed.jsx
+++ client/src/pages/user/Feed.jsx
@@ -26,34 +26,36 @@ export function Feed() {
   };
 
   return (
-    <main className={styles.mainContainer}>
-      <div className="container px-4">
-        <div className="row align-items-center g-lg-5">
-          <FeedForm />
-        </div>
-        <div className="row align-items-center g-lg-5 pt-5">
-          <div className="col-md-10 mx-auto col-lg-6 col-xl-6">
-            {posts.length === 0
-              ? empty
-              : posts.map((post) => <Post key={post.post_id} post={post} />)}
+    <div className={styles.pageWrapperFeed}>
+      <main className={styles.mainContainer}>
+        <div className="container px-4">
+          <div className="row align-items-center g-lg-5">
+            <FeedForm />
+          </div>
+          <div className="row align-items-center g-lg-5 pt-5">
+            <div className="col-md-10 mx-auto col-lg-6 col-xl-6">
+              {posts.length === 0
+                ? empty
+                : posts.map((post) => <Post key={post.post_id} post={post} />)}
+            </div>
+          </div>
+          <div className="row align-items-center g-lg-5 py-5">
+            {hasMorePosts ? (
+              <button
+                className="btn btn-primary"
+                onClick={handleLoadOlderPosts}
+                type="button"
+              >
+                Rodyti daugiau...
+              </button>
+            ) : (
+              <div className="alert alert-info text-center">
+                Daugiau turinio nėra.
+              </div>
+            )}
           </div>
         </div>
-        <div className="row align-items-center g-lg-5 py-5">
-          {hasMorePosts ? (
-            <button
-              className="btn btn-primary"
-              onClick={handleLoadOlderPosts}
-              type="button"
-            >
-              Rodyti daugiau...
-            </button>
-          ) : (
-            <div className="alert alert-info text-center">
-              Daugiau turinio nėra.
-            </div>
-          )}
-        </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }

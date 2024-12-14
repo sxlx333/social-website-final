82f321a8913a6dd3bac13b9d271040792e813876
sxlx333 <knifeq8@gmail.com>
2024-12-14 14:44:18 +0200

    about page section text & style

--- client/src/pages/public/AboutUs.jsx
+++ client/src/pages/public/AboutUs.jsx
@@ -8,14 +8,14 @@ export function AboutUs() {
           <div className="col-12">
             <h1 className={styles.aboutHeading}>Apie mus</h1>
             <p className={styles.aboutParagraph}>
-              Gandalizdis yra pilno funkcionalumo (full-stack) žiniatinklio
+              "Gandalizdis" yra pilno funkcionalumo (full-stack) žiniatinklio
               programa, leidžianti naudotojams kurti, valdyti ir sąveikauti su
               savo įrašais bei profiliais. Ši platforma sukurta naudojant tiek
               kliento, tiek serverio technologijas, užtikrinant sklandžią
               naudotojo patirtį.
             </p>
             <p className={styles.aboutParagraph}>
-              Naudodamiesi Gandalizdis, naudotojai gali lengvai:
+              Naudodamiesi "Gandalizdis", naudotojai gali lengvai:
             </p>
             <ul className={styles.aboutList}>
               <li className={styles.aboutListItem}>
@@ -33,6 +33,54 @@ export function AboutUs() {
                 kitų bendrinamus įrašus.
               </li>
             </ul>
+            <section className={styles.container}>
+              <h2 className={styles.subHeading}>Ką dar galima patobulinti ?</h2>
+              <p className={`${styles.aboutParagraph}`}>
+                Nors Gandalizdis jau teikia pagrindines socialinių tinklų
+                funkcijas, tačiau trūksta kai kurių pagrindinių funkcijų arba
+                jas galima patobulinti. Pagrindinės idėjos:
+              </p>
+              <ul className={styles.aboutList}>
+                <h3 className={styles.listHeading}>Įrašų sekcija</h3>
+                <li className={styles.aboutListItem}>
+                  Galimybė rašyti komentarus prie įrašų.
+                </li>
+                <li className={styles.aboutListItem}>
+                  Galimybė bendrinti įrašus naudojant unikalias nuorodas, kad
+                  būtų lengviau platinti už platformos ribų.
+                </li>
+                <li className={styles.aboutListItem}>
+                  Leisti naudotojams prie įrašų pridėti nuotraukas, GIF ir kitas
+                  medijas.
+                </li>
+                <li className={styles.aboutListItem}>
+                  Leisti vartotojams užvedus ant patinka mygtuko pasirinkti
+                  emocijas.
+                </li>
+                <h3 className={styles.listHeading}>Profilio sekcija</h3>
+                <li className={styles.aboutListItem}>
+                  Leisti vartotojams keisti slaptažodžius tiesiai iš savo
+                  profilio nustatymų.
+                </li>
+                <li className={styles.aboutListItem}>
+                  Leisti vartotojams atkurti slaptažodį, išsiunčiant nuorodą į
+                  vartotojo el. paštą.
+                </li>
+                <li className={styles.aboutListItem}>
+                  Suteikti galimybę vartotojams bendrinti savo profilį, sukurti
+                  nuorodą.
+                </li>
+                <h3 className={styles.listHeading}>Kiti patobulinimai:</h3>
+                <li className={styles.aboutListItem}>
+                  Vartotojo sąsajos tobulinimas, kad patirtis būtų skandli ir
+                  intuityvi.
+                </li>
+                <li className={styles.aboutListItem}>
+                  Užtikrinti, kad platforma būtų pritaikyta mobiliesiems
+                  įrenginiams ir prieinama visiems.
+                </li>
+              </ul>
+            </section>
           </div>
         </div>
       </section>

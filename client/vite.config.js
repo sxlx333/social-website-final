152448ec2863c416644dfe5043639c3c5bfea70b
sxlx333 <knifeq8@gmail.com>
2024-12-17 18:34:12 +0200

    heroku test

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure this matches Netlify's Publish directory
+    outDir: 'dist',
   },
+  base: '/social-website-final/',
 });
